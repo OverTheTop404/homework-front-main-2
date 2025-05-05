70cbe9d400e4bb490f9290f4ee7cf718b4415f01
OverTheTop404 <over_the_top_404@mail.ru>
2025-05-05 10:40:03 +0400

    HW5 is update

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import s from './App.module.css'
+import HW6 from "../s2-homeworks/hw06/HW6";
 import HW5 from "../s2-homeworks/hw05/HW5";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
             <HW5/>
+            {/*<HW6/>*/}
         </div>
     )
 }
