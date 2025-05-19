89b988d8c7f8ecd8f5b176753aab8cada49e255e
OverTheTop404 <over_the_top_404@mail.ru>
2025-05-19 12:12:05 +0400

    HW7 update

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -51,7 +51,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       className={finalRadioClassName}
                       type={'radio'}
                       // name, checked, value делают студенты
-                      name={o.id}
+                      name={'radio'}
                       value={o.id}
                       checked={value === o.id}
 
