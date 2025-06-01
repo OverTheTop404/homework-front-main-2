6a9d3a2bb28ef04f3425775a3c761f4012c3d918
OverTheTop404 <over_the_top_404@mail.ru>
2025-06-01 21:24:15 +0400

    HW9 update and done

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -36,7 +36,7 @@ function Clock() {
 
 
     const stringTime = `(${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()})` || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = `(${String(date.getUTCDate()).padStart(2, '0')}.${String(date.getUTCMonth()+1).padStart(2, '0')}.${date.getUTCFullYear()})` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringDate = `${String(date.getUTCDate()).padStart(2, '0')}.${String(date.getUTCMonth()+1).padStart(2, '0')}.${date.getUTCFullYear()}` || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = date.toLocaleString('en-EN', { weekday: 'long' }) || <br/> // пишут студенты
